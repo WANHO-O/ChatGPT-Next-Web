f7ba6f7581c079faf87f92619c92425bb3839bde
WANHO-O <1198229388@qq.com>
2024-03-13 23:14:11 +0800

    none

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -155,10 +155,10 @@ export function SideBar(props: { className?: string }) {
     >
       <div className={styles["sidebar-header"]} data-tauri-drag-region>
         <div className={styles["sidebar-title"]} data-tauri-drag-region>
-          NextChat
+          动放科技
         </div>
         <div className={styles["sidebar-sub-title"]}>
-          Build your own AI assistant.
+          享受自由而平等的人工智能
         </div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
